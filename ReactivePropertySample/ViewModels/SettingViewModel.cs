cb5e7da8c6870aa9dc932f14f4374e853efeb022
齋藤真治 <mqbdr314@gmail.com>
2021-02-12 22:42:48 +0900

    プロジェクト ファイルを追加します。

---
+++ ReactivePropertySample/ViewModels/SettingViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using ReactivePropertySample_M;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactivePropertySample_VVM.ViewModels
+{
+    public class SettingViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Disposeまとめてしてくれるやつ
+        private readonly CompositeDisposable disposable = new CompositeDisposable();
+
+        public void Dispose()
+        {
+            disposable.Clear();
+            disposable.Dispose();
+        }
+
+        private readonly SettingModel model;
+
+        public ReactiveProperty<int> ViewWidth { get; }
+        public ReactiveProperty<int> ViewHeight { get; }
+        public ReactiveProperty<int> ViewX { get; }
+        public ReactiveProperty<int> ViewY { get; }
+
+        public ReactiveProperty<string> Name { get; }
+
+        public ReadOnlyReactiveProperty<bool> IsGod { get; }
+        public ReadOnlyReactiveProperty<string> Rank { get; }
+        public ReadOnlyReactiveProperty<int> FontSize { get; }
+
+        public ReadOnlyReactiveProperty<string> ViewRectInfo { get; }
+
+        public ReactiveCommand SaveCommand { get; }
+
+        public SettingViewModel()
+        {
+            model = new();
+
+            ViewWidth = model.ViewWidth.ToReactiveProperty().AddTo(disposable);
+            ViewHeight = model.ViewHeight.ToReactiveProperty().AddTo(disposable);
+            ViewX = model.ViewX.ToReactiveProperty().AddTo(disposable);
+            ViewY = model.ViewY.ToReactiveProperty().AddTo(disposable);
+
+            ViewRectInfo = 
+                ViewWidth.CombineLatest(ViewHeight, ViewX, ViewY, (w, h, x, y) =>  $"W:{w}, H:{h}, X:{x}, Y{y}")
+                .ToReadOnlyReactiveProperty<string>().AddTo(disposable);
+
+            Name = model.Name.ToReactiveProperty<string>().AddTo(disposable);
+
+            IsGod = Name.Select(name => name == "ぽんた").ToReadOnlyReactiveProperty().AddTo(disposable);
+            Rank = IsGod.Select(isGod => isGod ? "神" : "✨🤪✨").ToReadOnlyReactiveProperty<string>().AddTo(disposable);
+            FontSize = IsGod.Select(isGod => isGod ? 80: 50).ToReadOnlyReactiveProperty().AddTo(disposable);
+
+            SaveCommand = new ReactiveCommand().WithSubscribe(model.SaveSettings);
+        }
+    }
+}
---
+++ ReactivePropertySample_M/Settings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactivePropertySample_M
+{
+    public class Settings
+    {
+        private static Settings? instance = new();
+        public static Settings Instance
+        {
+            get
+            {
+                if(instance is null)
+                {
+                    Load();
+                }
+                return instance!;
+            }
+        }
+
+        public Rectangle MainViewRect { get; set; } = new(0, 0, 800, 450);
+
+        public string Name { get; set; } = "ぽんた";
+
+        private Settings() { }
+
+        /// <summary>
+        /// 設定ファイルをデシリアライズしてインスタンスを生成
+        /// </summary>
+        /// <returns></returns>
+        private static void Load()
+        {
+            // 今回は面倒なのでnew
+            instance = new();
+        }
+
+        /// <summary>
+        /// 設定ファイルをシリアライズして保存
+        /// </summary>
+        internal static void Save()
+        {
+            Debug.WriteLine("保存したよ");
+        }
+    }
+}
